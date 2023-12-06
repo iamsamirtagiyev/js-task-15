55a8ebe40e6e2b2d379318cac2a79014c2984924
iamsamirtagiyev <stagiyev579@gmail.com>
2023-12-06 20:33:13 +0400

    js

--- add.js
+++ add.js
@@ -4,3 +4,24 @@ const productImg = document.querySelector('#file')
 const productName = document.querySelector('#name')
 const productDescroption = document.querySelector('#description')
 
+button.onclick = (e)=>{
+    e.preventDefault()
+    if(productImg.value != '' && productDescroption.value != '' && productName.value != ''){
+        let obj = {}
+        let name = productName.value.trim()
+        let description = productDescroption.value.trim()
+        let img = productImg.files[0]
+        const reader = new FileReader()
+        reader.readAsDataURL(img)
+        reader.onload = (e)=>{
+            obj = {
+                url: e.target.result,
+                name: name,
+                about: description
+            }
+            axios.post('http://localhost:3000/robots', obj)
+        }
+        window.location= './index.html'
+    }
+}
+
